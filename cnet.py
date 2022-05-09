f7c9dfed73a46a6059bb6b24bcf5bfdb9e82ed08
Areeb Fazli <areebfazli@gmail.com>
2022-05-09 12:53:15 +0530

    new commit

--- cnet.py
+++ cnet.py
@@ -34,7 +34,7 @@ def tech():
         # print(title, content)
         # print(link['href'])
 
-        c.execute('''INSERT or IGNORE INTO posts(title, content, link)VALUES(?,?,?)''', (title, content, link))
+        c.execute('''INSERT IGNORE INTO posts(title, content, link)VALUES(?,?,?)''', (title, content, link))
 
     conn.commit()
     conn.close()
