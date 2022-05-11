e6b74896a009cae837faa3d410a3fdb05cca8ad5
Areeb Fazli <areebfazli@gmail.com>
2022-05-11 16:27:34 +0530

    make it better

--- db.py
+++ db.py
@@ -9,7 +9,7 @@ conn.autocommit = True
 cursor = conn.cursor()
 
 #Preparing query to create a database
-sql ='''DROP TABLE employee'''
+sql ='''CREATE TABLE posts(id INTEGER PRIMARY KEY, title TEXT UNIQUE, content TEXT, link TEXT)'''
 
 #Creating a database
 cursor.execute(sql)
