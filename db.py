ea846195f7d4befcf83dde57d70d961c880094d3
Areeb Fazli <areebfazli@gmail.com>
2022-05-12 09:57:39 +0530

    make it better

--- db.py
+++ db.py
@@ -10,7 +10,7 @@ cursor = conn.cursor()
 
 #Preparing query to create a database
 # sql ='''CREATE TABLE POSTS(ID SERIAL PRIMARY KEY, TITLE TEXT UNIQUE, CONTENT TEXT, LINK TEXT)'''
-sql = '''DROP TABLE posts'''
+sql = '''DROP TABLE POSTS'''
 #Creating a database
 cursor.execute(sql)
 print("Database created successfully........")
